66be631cca4b19c00f84e0d971d3ea36c7f2e526
Daniele001 <danieleceribelli@gmail.com>
2021-04-20 01:02:29 +0200

    Modified data search

--- parser.py
+++ parser.py
@@ -1,10 +1,11 @@
 from bs4 import BeautifulSoup
 from manga import Manga
 import requests
+import re
 
 URL = "https://www.starcomics.com/catalogo-fumetti"
 page = requests.get(URL)
-soup = BeautifulSoup(page.content, "html.parser").find("tbody").find_all("tr")
+soup = BeautifulSoup(page.content, "html.parser").find("tbody")
 
 
 # This function format scraped elements
@@ -13,10 +14,13 @@ def format_text(element):
     return element.strip().replace("  ", "").splitlines()
 
 
-# This function adds manga to a list
-def create_manga(collection):
-    for el in soup:
+# This function adds manga to list
+def create_list(name: str):
+    lst = []
+    reg = "\b(?:" + name + ")"
+    for el in soup.find_all("tr", text=re.compile(reg)):
         link = el.find("a", href=True)
         data = list(filter(None, format_text(el)))
         manga = Manga(data[0], data[1], data[2], link)
-        collection.append(manga)
+        lst.append(manga)
+    return lst
